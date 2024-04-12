d9bff51b0c03e58b72ac0e22c6ff714dfd947c65
Abishek-007 <abishekxofficial987@gmail.com>
2024-04-12 12:41:26 +0545

    my first program using for model ST4003CEM

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,35 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("this is my first program");
+        /*
+         * 1. Write a program that prints Hello World! to the console. Save the program
+         * as Hello.java.
+         * 
+         * 2. Extend/Modify the above program to print two additional lines:
+         * 
+         * This is my first program
+         * 
+         * I am on module ST4003CEM
+         * 
+         * System.out.print (not println) and produces the same output.
+         * 
+         * 3. Write a program that prints the following output:
+         * 
+         * A "quoted" String is
+         * 
+         * 'much' better if you learn
+         * 
+         * the rules of "escape sequences."
+         * 
+         * Also, "" represents an empty
+         * 
+         * String. Don't forget: use \"
+         * 
+         * instead of " !
+         * 
+         * '' is not the same as "
+         * 
+         */
     }
 }
