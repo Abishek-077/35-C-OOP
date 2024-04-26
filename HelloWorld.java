6e9f0ba3e2ee1dc6b7193161a8fc65ceb23ea3b1
Abishek-077 <abishekxofficial987@gmail.com>
2024-04-26 22:36:44 +0545

    Task

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello word");
-        System.out.println("this is my first program \n an model ST4003CEM");
+        System.out.println("hello word\n");
+        System.out.println("this is my first program \n an model ST4003CEM\n");
 
         System.out.println(
-                "A \"quoted\" String is 'much' better if you learn the rules of \"escape sequences.\" Also, \"\" represents an empty String. Don't forget: use \\\" instead of \" ! '' is not the same as \"");
+                "A \"quoted\" String is 'much' better if you learn the rules of \"escape sequences.\" Also, \"\" represents an empty String. Don't forget: use \\\" instead of \" ! '' is not the same as \n");
 
         /*
          * 1. Write a program that prints Hello World! to the console. Save the program
@@ -50,12 +50,46 @@ public class HelloWorld {
         System.out.println("**");
         System.out.println("***");
         System.out.println("***");
-        System.out.println("*****");
+        System.out.println("*****\n");
 
+        /*
+         * 5. WAP prints the following pattern:
+         *********
+         *********
+         *********
+         ********* 
+         * 
+         * 
+         * 
+         */
         System.out.println("******");
         System.out.println("******");
         System.out.println("******");
-        System.out.println("******");
+        System.out.println("******\n");
+
+        /*
+         * WAP that prints the following output:
+         * 
+         * A well-formed Java program has a main method with { and } braces.
+         * 
+         * 
+         * 
+         * A System.out.println statement has ( and ) and usually a String that starts
+         * and ends with a " character.
+         * 
+         * (But we type \" instead!)
+         * 
+         * What is the difference between
+         * 
+         * a ' and a "?  Or between a " and a \"?
+         * 
+         * 
+         * 
+         * One is what we see when we're typing our program. The other is what appears
+         * on the "console."
+         */
+        System.out.println(
+                "A well-formed Java program has a main method with { and } braces\n.A System.out.println statement has ( and ) and usually a String that starts and ends with a \"character.(But we type \" instead!)What is the difference betweena ' and a \"? \" Or between a \" and a \"?\n\n One is what we see when we're typing our program. The other is what appears on the \"console.\"");
 
     }
 
