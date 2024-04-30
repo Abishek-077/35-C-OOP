7d021545bf58f78cc58a69683eee901369710a96
Abishek-077 <abishekxofficial987@gmail.com>
2024-04-30 12:41:02 +0545

    while loop

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,3 +1,22 @@
-public class WhileLoop {
+import java.util.Scanner;;
 
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count <= 5) {
+            System.out.println("while cout " + 2 * count);
+            count++;
+        }
+        Scanner myobj = new Scanner(System.in);
+        System.out.println("pleae eneter a number ");
+        int invalue = myobj.nextInt();
+        while (invalue < 0 || invalue > 100) {
+            System.out.println("please enetr a number betwween 0-100");
+            invalue = myobj.nextInt();
+
+        }
+        System.out.println("corrent condition" + invalue);
+        myobj.close();
+
+    }
 }
